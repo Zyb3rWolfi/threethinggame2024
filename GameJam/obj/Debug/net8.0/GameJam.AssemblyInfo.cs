0f8bad662f519dff1b37904ca98cdf0b2d1884bd
Zakk <z.amers-delph-2023@hull.ac.uk>
2024-10-11 12:34:45 +0100

    Made verlocity work lmao

--- GameJam/obj/Debug/net8.0/GameJam.AssemblyInfo.cs
+++ GameJam/obj/Debug/net8.0/GameJam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280af3d92dead4253c11d8f31b65e4190b43aa3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d644d54a2b3dd8cc4dde389a7ea47de0eb786bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameJam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
